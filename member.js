9468fbdf70f0171581a962f2d44fb712b158b7df
shidqiamrlhaq <2006099@itg.ac.id>
2023-10-28 14:40:15 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,3 +1,18 @@
 function SkillsMember() {
     this.skills = [];
+    this.addSkill = function(skill) {
+        this.skills.push(skill);
+    };
+    this.removeSkill = function(skill) {
+        var index = this.skills.indexOf(skill);
+        if (index >= 0) {
+            this.skills.splice(index, 1);
+        }
+    };
+    this.removeAllSkills = function() {
+        this.skills = [];
+    };
+    this.getSkills = function() {
+        return this.skills;
+    };
 }
