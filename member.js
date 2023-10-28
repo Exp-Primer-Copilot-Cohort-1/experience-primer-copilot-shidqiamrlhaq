bc4136b6d4ef915d2f8570ce40328697422e42bf
shidqiamrlhaq <2006099@itg.ac.id>
2023-10-28 14:36:48 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,10 +1,3 @@
 function SkillsMember() {
-    this.name = 'Member';
-    this.age = 20;
-    this.skills = ['HTML', 'CSS', 'JS'];
-    this.showSkills = function() {
-        this.skills.forEach(function(skill) {
-            console.log(`${this.name} knows ${skill}`);
-        }.bind(this));
-    }
+    this.skills = [];
 }
